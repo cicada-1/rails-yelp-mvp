aa2aa45191ff02e2b1cd0d38ed85a655d67eff62
Peter Wallace <peterwallace@hotmail.co.uk>
2021-02-17 12:24:32 +0000

    Entered validations for restaurant and review models

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,3 +1,6 @@
 class Review < ApplicationRecord
   belongs_to :restaurant
+
+  validates :content, :rating, presence: true
+  validates :rating, format: { with: /[0-5]/, message: "Please enter a rating from 0 to 5" }
 end
