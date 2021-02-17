9b4a22975a4287d7fd80372bb46c9014296b839a
Peter Wallace <peterwallace@hotmail.co.uk>
2021-02-17 12:37:55 +0000

    Changed review's rating type from string to integer

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_17_122948) do
+ActiveRecord::Schema.define(version: 2021_02_17_123611) do
 
   create_table "restaurants", force: :cascade do |t|
     t.string "name"
@@ -22,7 +22,7 @@ ActiveRecord::Schema.define(version: 2021_02_17_122948) do
   end
 
   create_table "reviews", force: :cascade do |t|
-    t.string "rating"
+    t.integer "rating"
     t.text "content"
     t.integer "restaurant_id", null: false
     t.datetime "created_at", precision: 6, null: false
