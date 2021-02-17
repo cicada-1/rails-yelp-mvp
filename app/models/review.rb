be25064e2311175e6bc802b79bd99b1f5156ef55
Peter Wallace <peterwallace@hotmail.co.uk>
2021-02-17 12:45:46 +0000

    Added validations to review rating thast only allow integers and restrict the length to one number

--- app/models/review.rb
+++ app/models/review.rb
@@ -2,5 +2,7 @@ class Review < ApplicationRecord
   belongs_to :restaurant
 
   validates :content, :rating, presence: true
-  validates :rating, format: { with: /[0-5]/, message: "Please enter a rating from 0 to 5" }
+  validates :rating, format: { with: /[0-5]/, message: "Please enter a rating from 0 to 5" },
+    numericality: { only_integer: true, message: "Please enter a rating from 0 to 5" },
+    length: { maximum: 1, message: "Please enter a rating from 0 to 5" }
 end
